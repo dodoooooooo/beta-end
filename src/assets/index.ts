43723bfc244302b61fc7f0cad0e266ac2916348f
yixuan325 <D1123998@o365.fcu.edu.tw>
2025-01-12 12:56:59 +0800

    update assert index

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,4 +4,16 @@ import Ring from './ring.png';
 import Down_Arrow from './arrow-drop-down.png';
 import Menu from './menu.svg';
 import Close from './close.svg';
-export { Logo, Search, Ring, Down_Arrow,Menu,Close };
+import Module from './module.svg';
+import Bootup from './bootup.svg';
+import User from './user.svg';
+import Disk from './disk.svg';
+import Cron from './cron.svg';
+import Software from './software.svg';
+import System from './system.svg';
+import Server from './server.svg';
+import Utils from './utils.svg';
+import Network from './network.svg';
+import Hardware from './hardware.svg';
+
+export { Logo, Search, Ring, Down_Arrow, Menu, Close, Module, Bootup, User, Disk, Cron, Software, System, Server, Utils, Network, Hardware };
