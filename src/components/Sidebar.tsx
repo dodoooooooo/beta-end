9c2aaf882b3e2c01d812ddd8e6498c1d0298556e
dodoooooooo <0979829234www@gmail.com>
2025-01-12 14:29:08 +0800

    新增sidebar 其餘欄位及滾輪

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { Logo, Close } from '@assets';
-import { HomeIcon, CogIcon } from '@heroicons/react/24/outline';
+import { HomeIcon, CogIcon, ComputerDesktopIcon, CubeIcon, PowerIcon, UserIcon, ServerIcon, ClockIcon, ArrowDownTrayIcon, BookOpenIcon, ServerStackIcon, WrenchIcon, GlobeAltIcon, CpuChipIcon } from '@heroicons/react/24/outline';
 import { useState, FC } from 'react';
 import { useDrag } from '@use-gesture/react';
 
@@ -34,7 +34,7 @@ const Sidebar: FC<SidebarProps> = ({ isOpen, onClose, setCurrentPage }) => {
   return (
     <div
       {...bind()}
-      className={`fixed inset-y-0 left-0 p-4 text-white bg-navbar transform ${
+      className={`fixed inset-y-0 left-0 p-4 text-white bg-navbar transform overflow-y-auto ${
         isOpen ? 'translate-x-0' : '-translate-x-full'
       } transition-transform duration-300 md:static md:translate-x-0 md:w-80`}
     >
@@ -66,6 +66,126 @@ const Sidebar: FC<SidebarProps> = ({ isOpen, onClose, setCurrentPage }) => {
           >
             <CogIcon className='w-5 h-5' />
             <span>Services</span>
+          </a>
+        </li>
+        <li className='mb-2'>
+          <a
+            href='#'
+            className={`${style.navStyle} ${active === 'CHM' ? style.activeStyle : style.non_activeStyle}`}
+            onClick={() => handlePageChange('CHM')}
+          >
+            <ComputerDesktopIcon className='w-5 h-5' />
+            <span>CHM</span>
+          </a>
+        </li>
+        <li className='mb-2'>
+          <a
+            href='#'
+            className={`${style.navStyle} ${active === 'Modules Configuration' ? style.activeStyle : style.non_activeStyle}`}
+            onClick={() => handlePageChange('Modules Configuration')}
+          >
+            <CubeIcon className='w-5 h-5' />
+            <span>Modules Configuration</span>
+          </a>
+        </li>
+        <li className='mb-2'>
+          <a
+            href='#'
+            className={`${style.navStyle} ${active === 'Bootup & Shutdown' ? style.activeStyle : style.non_activeStyle}`}
+            onClick={() => handlePageChange('Bootup & Shutdown')}
+          >
+            <PowerIcon className='w-5 h-5' />
+            <span>Bootup & Shutdown</span>
+          </a>
+        </li>
+        <li className='mb-2'>
+          <a
+            href='#'
+            className={`${style.navStyle} ${active === 'Users & Groups' ? style.activeStyle : style.non_activeStyle}`}
+            onClick={() => handlePageChange('Users & Groups')}
+          >
+            <UserIcon className='w-5 h-5' />
+            <span>Users & Groupsn</span>
+          </a>
+        </li>
+        <li className='mb-2'>
+          <a
+            href='#'
+            className={`${style.navStyle} ${active === 'Disk Management' ? style.activeStyle : style.non_activeStyle}`}
+            onClick={() => handlePageChange('Disk Management')}
+          >
+            <ServerIcon className='w-5 h-5' />
+            <span>Disk Management</span>
+          </a>
+        </li>
+        <li className='mb-2'>
+          <a
+            href='#'
+            className={`${style.navStyle} ${active === 'Cron Management' ? style.activeStyle : style.non_activeStyle}`}
+            onClick={() => handlePageChange('Cron Management')}
+          >
+            <ClockIcon className='w-5 h-5' />
+            <span>Cron Management</span>
+          </a>
+        </li>
+        <li className='mb-2'>
+          <a
+            href='#'
+            className={`${style.navStyle} ${active === 'Software Package' ? style.activeStyle : style.non_activeStyle}`}
+            onClick={() => handlePageChange('Software Package')}
+          >
+            <ArrowDownTrayIcon className='w-5 h-5' />
+            <span>Software Package</span>
+          </a>
+        </li>
+        <li className='mb-2'>
+          <a
+            href='#'
+            className={`${style.navStyle} ${active === 'System Logs' ? style.activeStyle : style.non_activeStyle}`}
+            onClick={() => handlePageChange('System Logs')}
+          >
+            <BookOpenIcon className='w-5 h-5' />
+            <span>System Logs</span>
+          </a>
+        </li>
+        <li className='mb-2'>
+          <a
+            href='#'
+            className={`${style.navStyle} ${active === 'Servers' ? style.activeStyle : style.non_activeStyle}`}
+            onClick={() => handlePageChange('Servers')}
+          >
+            <ServerStackIcon className='w-5 h-5' />
+            <span>Servers</span>
+          </a>
+        </li>
+        <li className='mb-2'>
+          <a
+            href='#'
+            className={`${style.navStyle} ${active === 'Utils' ? style.activeStyle : style.non_activeStyle}`}
+            onClick={() => handlePageChange('Utils')}
+          >
+            <WrenchIcon className='w-5 h-5' />
+            <span>Utils</span>
+          </a>
+        </li>
+        <li className='mb-2'>
+          <a
+            href='#'
+            className={`${style.navStyle} ${active === 'Networks' ? style.activeStyle : style.non_activeStyle}`}
+            onClick={() => handlePageChange('Networks')}
+          >
+            <GlobeAltIcon className='w-5 h-5' />
+            <span>Networks</span>
+          </a>
+        </li>
+        <li className='mb-2'>
+          <a
+            href='#'
+            className={`${style.navStyle} ${active === 'Hardware' ? style.activeStyle : style.non_activeStyle}`}
+            onClick={() => handlePageChange('Hardware')}
+          >
+            <CpuChipIcon className='w-5 h-5' />
+            <span>Hardware</span>
           </a>
         </li>
       </ul>
